27a4c530be883d2d11d91aecb022e86fc11f1ebc
seryocardu <seryocardu@gmail.com>
2021-09-24 13:49:56 +0200

    feature core components

--- projects/core-library/src/lib/core-library.module.ts
+++ projects/core-library/src/lib/core-library.module.ts
@@ -1,17 +1,27 @@
 import { NgModule } from '@angular/core';
+import { FormButtonComponent } from './components/form-button/form-button.component';
+import { FormInputComponent } from './components/form-input/form-input.component';
 import { MenuComponent } from './components/menu/menu.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 
 
 
 @NgModule({
   declarations: [
-    MenuComponent
+    MenuComponent,
+    FormButtonComponent,
+    FormInputComponent,
+    NavbarComponent
+
   ],
   imports: [
   ],
   exports: [
-    MenuComponent
+    MenuComponent,
+    FormButtonComponent,
+    FormInputComponent,
+    NavbarComponent
   ]    
 })
 
